081912b0ff60c9990c460accb4894c3c4cd63935
Koen van Marrewijk <koenvanmarrewijk@gmail.com>
2019-03-27 11:39:57 +0100

    Added services for main

---
+++ main/src/main/java/backend/classroom/service/ClassroomService.java
@@ -0,0 +1,4 @@
+package backend.classroom.service;
+
+public interface ClassroomService {
+}
---
+++ main/src/main/java/backend/exam/service/ExamServiceImpl.java
@@ -0,0 +1,4 @@
+package backend.exam.service;
+
+public class ExamServiceImpl implements ExamService {
+}
